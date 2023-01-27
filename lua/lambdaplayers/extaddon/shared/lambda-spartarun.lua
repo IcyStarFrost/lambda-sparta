0139cd367561cb5506d47d323037b36a40f222fa
IcyStarFrost <frostlegion10@gmail.com>
2023-01-27 14:01:59 -0600

    Updated to Lambda's new external addon system

---
+++ lua/lambdaplayers/extaddon/shared/lambda-spartarun.lua
@@ -0,0 +1,2 @@
+
+CreateLambdaConvar( "lambdaplayers_sparta_aftertrack", "lambdasparta/aftertrack.mp3", true, false, false, "The sound file to play after Sparta Punch has been successfully used", nil, nil, { type = "Text", name = "Sparta Punch AfterTrack", category = "Weapon Utilities" } )
